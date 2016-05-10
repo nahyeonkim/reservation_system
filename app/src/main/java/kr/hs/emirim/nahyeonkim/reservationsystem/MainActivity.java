aec8e7d9aa3ebbce40993d8c585899b694c597e9
Nahyeon Kim <nah0722@naver.com>
2016-05-10 19:32:24 +0900

    #1 make

---
+++ app/src/main/java/kr/hs/emirim/nahyeonkim/reservationsystem/MainActivity.java
@@ -0,0 +1,40 @@
+package kr.hs.emirim.nahyeonkim.reservationsystem;
+
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    Chronometer chrono;
+    Button butStart,butDone;
+    RadioButton radioDate, radioTime;
+    CalendarView calView;
+    TimePicker timePick;
+    TextView textResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chrono=(Chronometer)findViewById(R.id.chrono);
+        butStart=(Button)findViewById(R.id.but_start);
+        butDone=(Button)findViewById(R.id.but_done);
+        radioDate=(RadioButton)findViewById(R.id.radio_date);
+        radioDate=(RadioButton)findViewById(R.id.radio_time);
+        calView=(CalendarView)findViewById(R.id.calendar);
+        timePick=(TimePicker)findViewById(R.id.time_pick);
+        textResult=(TextView)findViewById(R.id.text_result);
+        timePick.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
+    }
+}
